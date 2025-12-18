5cfde64faa69db526d7c45b49eb7976c9d3dd785
syihwamoza <mozakastella26@gmail.com>
2025-12-18 22:44:07 +0700

    Menambahkan DestinasiHome sebagai konfigurasi rute navigasi dan judul halaman untuk layar utama (Home)

---
+++ app/src/main/java/com/example/pertemuan12/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.pertemuan12.uicontroller.route
+
+import com.example.pertemuan12.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
