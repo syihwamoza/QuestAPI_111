1f34c27ec71a9854c554bbfe632a9c8e2056c444
syihwamoza <mozakastella26@gmail.com>
2025-12-26 17:38:24 +0700

    Menambahkan title dan argument idSiswa pada DestinasiEdit

--- app/src/main/java/com/example/pertemuan12/uicontroller/route/DestinasiEdit.kt
+++ app/src/main/java/com/example/pertemuan12/uicontroller/route/DestinasiEdit.kt
@@ -4,5 +4,9 @@ import com.example.pertemuan12.R
 
 object DestinasiEdit: DestinasiNavigasi {
     override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg"
+
 
 }
