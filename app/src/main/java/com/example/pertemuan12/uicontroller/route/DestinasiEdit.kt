65fc9e018e1d8f48b7f9ca89c054d8b06fa03375
syihwamoza <mozakastella26@gmail.com>
2025-12-26 17:37:55 +0700

    Menambahkan DestinasiEdit untuk navigasi halaman edit

--- app/src/main/java/com/example/pertemuan12/uicontroller/route/DestinasiEdit.kt
+++ app/src/main/java/com/example/pertemuan12/uicontroller/route/DestinasiEdit.kt
@@ -4,7 +4,5 @@ import com.example.pertemuan12.R
 
 object DestinasiEdit: DestinasiNavigasi {
     override val route = "item_edit"
-    override val titleRes = R.string.edit_siswa
-    const val itemIdArg = "idSiswa"
-    val routeWithArgs = "$route/{$itemIdArg"
+
 }
