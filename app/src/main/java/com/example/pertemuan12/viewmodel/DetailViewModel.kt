e6303b37d24bd459e369cba7f4c9e844ae021fe9
syihwamoza <mozakastella26@gmail.com>
2025-12-26 17:39:51 +0700

    Menambahkan StatusUIDetail dengan state Success

--- app/src/main/java/com/example/pertemuan12/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/pertemuan12/viewmodel/DetailViewModel.kt
@@ -17,45 +17,4 @@ import java.io.IOException
 
 sealed interface StatusUIDetail {
     data class Success(val satusiswa: DataSiswa) : StatusUIDetail
-    object Error : StatusUIDetail
-    object Loading : StatusUIDetail
-}
-class DetailViewModel(savedStateHandle: SavedStateHandle, private val repositoryDataSiswa:
-RepositoryDataSiswa
-): ViewModel() {
-
-    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
-    var statusUIDetail:StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
-        private set
-
-    init {
-        getSatuSiswa()
-    }
-
-    fun getSatuSiswa(){
-        viewModelScope.launch {
-            statusUIDetail = StatusUIDetail.Loading
-            statusUIDetail = try {
-                StatusUIDetail.Success(satusiswa = repositoryDataSiswa.getSatuSiswa(idSiswa))
-            }
-            catch (e: IOException){
-                StatusUIDetail.Error
-            }
-            catch (e: HttpException){
-                StatusUIDetail.Error
-            }
-        }
-    }
-
-    @SuppressLint("SuspiciousIndentation")
-    suspend fun hapusSatuSiswa() {
-        val resp: Response<Void> = repositoryDataSiswa.hapusSatuSiswa(idSiswa)
-
-        if (resp.isSuccessful){
-            println("Sukses Hapus Data : ${resp.message()}")
-        }else{
-            println("Gagal Hapus Data : ${resp.errorBody()}")
-        }
-    }
-
 }
