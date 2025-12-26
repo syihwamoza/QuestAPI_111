abd49daf1c285632bee4de8b8bbea8e87432bfcd
syihwamoza <mozakastella26@gmail.com>
2025-12-26 17:45:16 +0700

    Menambahkan handling response pada fungsi hapusSatuSiswa

--- app/src/main/java/com/example/pertemuan12/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/pertemuan12/viewmodel/DetailViewModel.kt
@@ -49,5 +49,14 @@ RepositoryDataSiswa
     }
     @SuppressLint("SuspiciousIndentation")
     suspend fun hapusSatuSiswa() {
+        val resp: Response<Void> = repositoryDataSiswa.hapusSatuSiswa(idSiswa)
 
+        if (resp.isSuccessful){
+            println("Sukses Hapus Data : ${resp.message()}")
+        }else{
+            println("Gagal Hapus Data : ${resp.errorBody()}")
+        }
+    }
+
+}
 
