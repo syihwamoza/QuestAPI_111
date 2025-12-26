68b5e32493d5c5801f6dc39671c8b841f76bdabb
syihwamoza <mozakastella26@gmail.com>
2025-12-26 17:41:56 +0700

    Menambahkan DetailViewModel untuk halaman detail siswa

--- app/src/main/java/com/example/pertemuan12/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/pertemuan12/viewmodel/DetailViewModel.kt
@@ -19,5 +19,8 @@ sealed interface StatusUIDetail {
     data class Success(val satusiswa: DataSiswa) : StatusUIDetail
     object Error : StatusUIDetail
     object Loading : StatusUIDetail
-
 }
+
+class DetailViewModel(savedStateHandle: SavedStateHandle, private val repositoryDataSiswa:
+RepositoryDataSiswa
+): ViewModel()
