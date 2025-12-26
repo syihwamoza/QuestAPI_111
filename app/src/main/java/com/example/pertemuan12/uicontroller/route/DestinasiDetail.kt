075c3c76025bf2fae3ecbca57abc691d6a195f2a
syihwamoza <mozakastella26@gmail.com>
2025-12-26 17:36:59 +0700

    Menambahkan argument idSiswa pada route navigasi

--- app/src/main/java/com/example/pertemuan12/uicontroller/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/pertemuan12/uicontroller/route/DestinasiDetail.kt
@@ -5,5 +5,7 @@ import com.example.pertemuan12.R
 object DestinasiDetail: DestinasiNavigasi {
     override val route = "detail_siswa"
     override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg"
 
 }
