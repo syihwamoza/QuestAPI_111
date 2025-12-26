e56d59acaa4cb736139ab39b8f1bef189d8f8961
syihwamoza <mozakastella26@gmail.com>
2025-12-26 17:49:24 +0700

    Menambahkan fungsi validasi input data siswa

--- app/src/main/java/com/example/pertemuan12/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/pertemuan12/viewmodel/EditViewModel.kt
@@ -39,6 +39,12 @@ class EditViewModel(
             UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
     }
 
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+
+    }
 }
 
 
